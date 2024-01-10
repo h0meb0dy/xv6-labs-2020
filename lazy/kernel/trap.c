b6c39a7956238ade6ef4fe36316d775d2da7fae0
조성준 (Seongjoon Cho) <h0meb0dysj@gmail.com>
2024-01-10 09:45:14 +0900

    [lazy] Lazy allocation

--- lazy/kernel/trap.c
+++ lazy/kernel/trap.c
@@ -61,6 +61,18 @@ void usertrap(void) {
         syscall();
     } else if ((which_dev = devintr()) != 0) {
         // ok
+    } else if (r_scause() == 13 || r_scause() == 15) {
+        uint64 va = PGROUNDDOWN(r_stval()); // address of faulted page
+        char *mem = kalloc();
+        if (!mem)
+            p->killed = 1;
+        else {
+            memset(mem, 0, PGSIZE);
+            if (mappages(p->pagetable, va, PGSIZE, (uint64)mem, PTE_W | PTE_X | PTE_R | PTE_U)) {
+                kfree(mem);
+                p->killed = 1;
+            }
+        }
     } else {
         printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
         printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
