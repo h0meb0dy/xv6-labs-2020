400efc656e21d62940b08fbf42c73f49ed34fad2
조성준 (Seongjoon Cho) <h0meb0dysj@gmail.com>
2024-01-10 09:47:33 +0900

    [lazy] Lazy allocation

--- lazy/kernel/sysproc.c
+++ lazy/kernel/sysproc.c
@@ -43,8 +43,8 @@ sys_sbrk(void) {
     if (argint(0, &n) < 0)
         return -1;
     // addr = myproc()->sz;
-    if (growproc(n) < 0)
-        return -1;
+    // if (growproc(n) < 0)
+    //     return -1;
     addr = p->sz; // old size
     p->sz += n;   // increment process size
     return addr;
