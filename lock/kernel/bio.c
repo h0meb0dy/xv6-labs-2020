f46a6781094142bf26587e2e1b3680076b262d89
조성준 (Seongjoon Cho) <h0meb0dysj@gmail.com>
2024-01-28 13:07:38 +0900

    [lock] Buffer cache

--- lock/kernel/bio.c
+++ lock/kernel/bio.c
@@ -23,31 +23,41 @@
 #include "fs.h"
 #include "buf.h"
 
+#define NBUCKET 5
+#define LOCKNAME_MAX 0x10
 struct {
     struct spinlock lock;
+    char lockname[LOCKNAME_MAX + 1];
     struct buf buf[NBUF];
 
     // Linked list of all buffers, through prev/next.
     // Sorted by how recently the buffer was used.
     // head.next is most recent, head.prev is least.
     struct buf head;
-} bcache;
+} bcache[NBUCKET];
 
 void binit(void) {
     struct buf *b;
 
-    initlock(&bcache.lock, "bcache");
+    for (int i = 0; i < NBUCKET; i++) {
+        snprintf(bcache[i].lockname, LOCKNAME_MAX, "bcache%d", i + 1); // bcache1, bcache2, bcache3, ...
+        initlock(&bcache[i].lock, bcache[i].lockname);
 
-    // Create linked list of buffers
-    bcache.head.prev = &bcache.head;
-    bcache.head.next = &bcache.head;
-    for (b = bcache.buf; b < bcache.buf + NBUF; b++) {
-        b->next = bcache.head.next;
-        b->prev = &bcache.head;
-        initsleeplock(&b->lock, "buffer");
-        bcache.head.next->prev = b;
-        bcache.head.next = b;
+        // Create linked list of buffers
+        bcache[i].head.prev = &bcache[i].head;
+        bcache[i].head.next = &bcache[i].head;
+        for (b = bcache[i].buf; b < bcache[i].buf + NBUF; b++) {
+            b->next = bcache[i].head.next;
+            b->prev = &bcache[i].head;
+            initsleeplock(&b->lock, "buffer");
+            bcache[i].head.next->prev = b;
+            bcache[i].head.next = b;
+        }
     }
+}
+
+int bcache_idx(uint dev, uint blockno) {
+    return (((uint64)dev << 32) | blockno) % NBUCKET;
 }
 
 // Look through buffer cache for block on device dev.
@@ -57,13 +67,15 @@ static struct buf *
 bget(uint dev, uint blockno) {
     struct buf *b;
 
-    acquire(&bcache.lock);
+    int idx = bcache_idx(dev, blockno);
+
+    acquire(&bcache[idx].lock);
 
     // Is the block already cached?
-    for (b = bcache.head.next; b != &bcache.head; b = b->next) {
+    for (b = bcache[idx].head.next; b != &bcache[idx].head; b = b->next) {
         if (b->dev == dev && b->blockno == blockno) {
             b->refcnt++;
-            release(&bcache.lock);
+            release(&bcache[idx].lock);
             acquiresleep(&b->lock);
             return b;
         }
@@ -71,13 +83,13 @@ bget(uint dev, uint blockno) {
 
     // Not cached.
     // Recycle the least recently used (LRU) unused buffer.
-    for (b = bcache.head.prev; b != &bcache.head; b = b->prev) {
+    for (b = bcache[idx].head.prev; b != &bcache[idx].head; b = b->prev) {
         if (b->refcnt == 0) {
             b->dev = dev;
             b->blockno = blockno;
             b->valid = 0;
             b->refcnt = 1;
-            release(&bcache.lock);
+            release(&bcache[idx].lock);
             acquiresleep(&b->lock);
             return b;
         }
@@ -111,31 +123,35 @@ void brelse(struct buf *b) {
     if (!holdingsleep(&b->lock))
         panic("brelse");
 
+    int idx = bcache_idx(b->dev, b->blockno);
+
     releasesleep(&b->lock);
 
-    acquire(&bcache.lock);
+    acquire(&bcache[idx].lock);
     b->refcnt--;
     if (b->refcnt == 0) {
         // no one is waiting for it.
         b->next->prev = b->prev;
         b->prev->next = b->next;
-        b->next = bcache.head.next;
-        b->prev = &bcache.head;
-        bcache.head.next->prev = b;
-        bcache.head.next = b;
+        b->next = bcache[idx].head.next;
+        b->prev = &bcache[idx].head;
+        bcache[idx].head.next->prev = b;
+        bcache[idx].head.next = b;
     }
 
-    release(&bcache.lock);
+    release(&bcache[idx].lock);
 }
 
 void bpin(struct buf *b) {
-    acquire(&bcache.lock);
+    int idx = bcache_idx(b->dev, b->blockno);
+    acquire(&bcache[idx].lock);
     b->refcnt++;
-    release(&bcache.lock);
+    release(&bcache[idx].lock);
 }
 
 void bunpin(struct buf *b) {
-    acquire(&bcache.lock);
+    int idx = bcache_idx(b->dev, b->blockno);
+    acquire(&bcache[idx].lock);
     b->refcnt--;
-    release(&bcache.lock);
+    release(&bcache[idx].lock);
 }
